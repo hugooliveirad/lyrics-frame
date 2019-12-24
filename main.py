97e1c7b7e8ee6d2b5af801e1f88b0fe11058b1cb
Hugo Bessa <eu@hugobessa.com.br>
2019-12-23 21:27:06 -0300

    Makes the main.py script select an image

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os
+from datetime import datetime
 from inky import InkyPHAT
 from PIL import Image, ImageDraw
 
 inky_display = InkyPHAT('black')
 
-img = Image.open('./images/deixaeulheconvencer.png')
+imgs_dir = "/home/pi/lyrics-frame/images"
+
+imgs = [os.path.join(imgs_dir, f) for f in os.listdir(imgs_dir) if os.path.isfile(os.path.join(imgs_dir, f))]
+
+day_of_year = datetime.now()
+
+img_of_the_day = day_of_year % len(imgs)
+
+img = Image.open(imgs[img_of_the_day])
 
 inky_display.set_image(img)
 inky_display.show()
---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(
+    name='lyrics-frame',
+    version='0.0.0',
+    author='Hugo Bessa',
+    author_email='eu@hugobessa.com.br',
+    license='MIT'
+)
